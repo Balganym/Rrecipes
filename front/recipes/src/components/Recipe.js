046eca49ff06a533cba026e3d1efb15b3f969d8d
KhanNrd <nurdek98@gmail.com>
2018-02-25 17:22:30 +0600

    Bako Ne krichi

---
+++ front/recipes/src/components/Recipe.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import IconButton from 'material-ui/IconButton';
+
+class Recipe extends Component {
+   constructor(props){
+      super(props)
+      this.state = ({
+         recipe: "",
+         step: "",
+         recipes: [],
+         method: []
+      })
+   }
+
+   render() {
+      return(
+         <div className = "Recipe"> 
+         
+         </div>
+      );
+   }
+}
+
+
+export default Recipe;
