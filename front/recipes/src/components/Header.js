fc246420561223a0be76baf7f284f68202ec0d5f
Balganym <bakosa_98@mail.ru>
2018-02-25 16:47:06 +0600

    start

--- front/recipes/src/components/Header.js
+++ front/recipes/src/components/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
   render() {
     return (
       <div className="Header">
-        
+        Header is here
       </div>
     );
   }
--- front/recipes/src/index.js
+++ front/recipes/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import Header from './components/Header';
 
 ReactDOM.render(
-  <App />,
+  <Header />,
   document.getElementById('root')
 );
